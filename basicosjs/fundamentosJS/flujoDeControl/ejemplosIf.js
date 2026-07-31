2a732cd7a30a26e57e3134dd8691290583a806a6
Mishelle Bohorquez <117234657+MishelleBohorquez@users.noreply.github.com>
2026-07-31 14:45:32 -0500

    Delete basicosjs directory

--- basicosjs/fundamentosJS/flujoDeControl/ejemplosIf.js
+++
@@ -1,9 +0,0 @@
-// If básico = condicionales
-let totalPedido = 90000;
-if (totalPedido > 80000){
-    console.log("Domicilio gratis + bono 🎁");
-} else if (totalPedido > 50000) {
-    console.log("Domicilio GRATIS 🎉")
-} else {
-    console.log("Domicilio: $5000");
-}
--- basicosjs/fundamentosJS/flujoDeControl/ejemplosWhile.js
+++
@@ -1,6 +0,0 @@
-let repatidoresLibres = 1;
-
-while (repatidoresLibres <= 3){
-    console.log("Repartidor #", repatidoresLibres , "disponible");
-    repatidoresLibres = repatidoresLibres + 1;
-}
